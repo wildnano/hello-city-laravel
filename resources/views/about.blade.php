14f241422da3813fbd7caeb4347a236f1b4e60c5
wildnano <wildnano33@gmail.com>
2020-11-10 22:49:15 +0100

    Add about page and update links

---
+++ resources/views/about.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>About Us | Hello City</title>
+</head>
+
+<body>
+    <p>Build by &hearts; by Wildnano.</p>
+    <p><a href="/">Revenir à l'accueil</a></p>
+    <footer>
+        <p>&copy; Copyright {{ date('Y') }} &middot; <a href="/about-us">About Us</a></p>
+    </footer>
+</body>
+
+</html>
