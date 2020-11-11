7a234f2751ba5c354846231609768398e29734ee
wildnano <wildnano33@gmail.com>
2020-11-11 19:34:30 +0100

    Make us of asset function help to load images

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,7 +1,7 @@
 @extends('app')
 
 @section('content')
-<img src="/images/drapeau_madinina01.png" alt="Madinina Flag">
+<img src="{{ asset('/images/drapeau_madinina01.png') }}" alt="Madinina Flag">
     <h1>Hello from Madinina !</h1>
     <p>It's currently {{ date('h:i A') }}.</p>
 @endsection
