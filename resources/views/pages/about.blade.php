c2540fd99e92ca51e586ea779ff2d2dc6e0619b8
wildnano <wildnano33@gmail.com>
2020-11-11 17:50:34 +0100

    Add images

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,6 +3,7 @@
 @section('title','About Us | ' . config('app.name'))
 
 @section('content')
+<img src="/images/favicon.png" alt="Wildnano">
     <p>Build by &hearts; by Wildnano.</p>
     <p><a href="{{ route('app_home') }}">Revenir à l'accueil</a></p>
 @endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,6 +1,7 @@
 @extends('app')
 
 @section('content')
+<img src="/images/drapeau_madinina01.png" alt="Madinina Flag">
     <h1>Hello from Madinina !</h1>
     <p>It's currently {{ date('h:i A') }}.</p>
 @endsection
