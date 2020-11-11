e0adb4dbbdd72457501e59886215db5ed92fc9bc
wildnano <wildnano33@gmail.com>
2020-11-11 12:12:42 +0100

    Remove duplication by using app layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,19 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>About Us | Hello City</title>
-</head>
-
-<body>
+@section('content')
     <p>Build by &hearts; by Wildnano.</p>
     <p><a href="/">Revenir à l'accueil</a></p>
-    <footer>
-        <p>&copy; Copyright {{ date('Y') }}</a></p>
-    </footer>
-</body>
-
-</html>
+@endsection
